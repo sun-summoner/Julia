fb085932982ef0e8e1446bea5474cd124763d67b
sun-summoner <90707814+sun-summoner@users.noreply.github.com>
2023-09-14 21:40:45 +0300

    Update Robot.jl

--- Robot.jl
+++ Robot.jl
@@ -1,5 +1,5 @@
 using HorizonSideRobots
-
+HSR = HorizonSideRobots
 
 r = Robot(animate=true)
 function markline!(robot, side)
@@ -11,11 +11,21 @@ function markline!(robot, side)
     end
     return c
 end
+
+
 function go_steps!(robot, side, steps)
     for _ in 1:steps
         move!(robot, side)
     end
 end
+
+
+function HSR.move!(robot, sides)
+    for s in sides
+        move!(robot, s)
+    end
+end
+
 
 function go_along!(robot, side)
     num_side_steps = 0
@@ -27,21 +37,19 @@ function go_along!(robot, side)
 end
 
 
+function HSR.isborder(robot, side::Tuple{HorizonSide, HorizonSide})
+    return isborder(robot, side[1]) || isborder(robot, side[2])
+end
+
+
 function inverse(side)
     return HorizonSide((Int(side) + 2) % 4)
 end
 
-
-function mark_diagonal!(robot, side1, side2)
-    c = 0
-    while !(isborder(robot, side1) || isborder(robot, side2))
-        c += 1
-        move!(robot, side1)
-        move!(robot, side2)
-        putmarker!(robot)
-    end
-    return c
+function inverse(s::Tuple{HorizonSide, HorizonSide})
+    return map(inverse, s)
 end
+
 
 
 function straight_cross!(robot)
@@ -66,7 +74,7 @@ end
 function diagonal_cross!(robot)
     for side1 in (Nord, Sud)
         for side2 in (West, Ost)
-            count = mark_diagonal!(robot, side1, side2)
+            count = markline!(robot, (side1, side2))
             go_steps!(robot, inverse(side1), count)
             go_steps!(robot, inverse(side2), count)
         end
@@ -93,16 +101,14 @@ function whole_field!(robot)
 end
 
 
-function go_along_with_check!(robot, side)
-    flag = false
+function go_along_with_check!(robot, side, side_of_check)
     while !isborder(robot, side)
-            flag = isborder(robot, Nord)
-            if flag
-                return flag
+            if isborder(robot, side_of_check)
+                return true
             end
             move!(robot, side)
     end
-    return flag
+    return false
 end
 
 function mark_boarder!(robot, side_check, side_go)
@@ -115,22 +121,26 @@ end
 
 function perimetr_of_frames!(robot)
     num_west_steps, num_sud_steps = go_along!(robot, West), go_along!(robot, Sud)
-    
+    num_west_steps += go_along!(robot, West)
     perimetr!(robot)
-    
-    while !go_along_with_check!(robot, Ost)
-        go_along!(robot, West)
-        move!(robot, Nord)
+
+    i = 3
+    side = HorizonSide((i + 1) % 4)
+    while !go_along_with_check!(robot, HorizonSide(i), side)
+        i = (i + 2) % 4 
+        move!(robot, side)
     end
-    move!(robot, West)
-    for side in (Ost,Nord,West,Sud)
+
+    move!(robot, HorizonSide((i + 2) % 4))
+    for side in (HorizonSide(i), HorizonSide((i + 1) % 4), HorizonSide((i + 2) % 4), HorizonSide((i + 3) % 4))
         side_go, side_check = side, HorizonSide((Int(side) + 1) % 4)
         putmarker!(robot)
         move!(robot, side_go)
         mark_boarder!(robot, side_check, side_go)
     end
 
-    go_along!(robot, West), go_along!(robot, Sud)
+    go_along!(robot, West)
+    go_along!(robot, Sud)
     go_steps!(robot, Nord, num_sud_steps)
     go_steps!(robot, Ost, num_west_steps)
     
